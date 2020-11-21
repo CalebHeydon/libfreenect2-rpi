f48a521d6f46ded11ba31f0296b9281cdfd14200
Caleb Heydon <caleb.heydon@gmail.com>
2020-11-21 14:31:50 -0500

    Initial commit

---
+++ install.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+if command -v clang
+then
+	C_COMPILER=clang
+else
+	C_COMPILER=gcc
+fi
+
+if command -v clang++
+then
+	CXX_COMPILER=clang++
+else
+	CXX_COMPILER=g++
+fi
+
+if command -v ld.lld
+then
+	FLAGS="-fuse-ld=lld"
+else
+	FLAGS=""
+fi
+
+if test -f "/usr/lib/libc++.so.1"
+then
+	CXX_FLAGS="-stdlib=libc++"
+else
+	CXX_FLAGS=""
+fi
+
+git clone https://github.com/CalebHeydon/libfreenect2
+cd libfreenect2
+
+mkdir build
+cd build
+cmake \
+	-DCMAKE_BUILD_TYPE=Release \
+	-DCMAKE_C_COMPILER=$C_COMPILER \
+	-DCMAKE_CXX_COMPILER=$CXX_COMPILER \
+	-DCMAKE_C_FLAGS="$FLAGS" \
+	-DCMAKE_CXX_FLAGS="$FLAGS $CXX_FLAGS" \
+	..
+make
+sudo make install
